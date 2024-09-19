b5d4ee7f8af40e3cfecea29ec5cf772ba43ba879
Timothy <timothysuldayan1@gmail.com>
2024-09-19 11:50:57 -0700

    gateway routing and token filtering configured

---
+++ gateway/src/main/java/com/example/gateway/config/RouterValidator.java
@@ -0,0 +1,24 @@
+package com.example.gateway.config;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+// This component is used for specifying any endpoints that we want to be public
+// without any jwt token claim or authorization
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
